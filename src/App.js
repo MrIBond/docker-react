56133097c62f25a0799a745c6d07499227bcfa03
Igor Bondarenko <bondarenkoigor1993@gmail.com>
2022-09-29 12:32:09 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi asa asass sadsdas as asds
         </p>
         <a
           className="App-link"
